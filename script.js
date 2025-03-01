48273300b44c58928b70f37586460d78fec4225d
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-03-01 08:12:29 +0300

    Adding scripts

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
     const bar = document.getElementById('bar');
     const close = document.getElementById('close');
+    const nav = document.getElementById('nav');
 })
